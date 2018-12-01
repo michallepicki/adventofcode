7b66bd389bbddd97e036bd8fbc36afe1a96dd580
Michał Łępicki <michal.lepicki@gmail.com>
2018-12-01 19:12:11 +0100

    add cpp solution

---
+++ 2018/cpp/1_chronal_calibration.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <utility>
+
+int main()
+{
+  int freq_change, freq = 0;
+  std::ifstream input_file("../1");
+  while (input_file >> freq_change)
+    freq += freq_change;
+  std::cout << freq << std::endl;
+
+  freq = 0;
+  bool found = false;
+  std::set<int> reached_freqs = {0};
+  while (!found)
+  {
+    input_file.clear();
+    input_file.seekg(0);
+    while (!found && input_file >> freq_change){
+      freq += freq_change;
+      auto result = reached_freqs.insert(freq);
+      if(!result.second)
+        found = true;
+    }
+  }
+  std::cout << freq << std::endl;
+  return 0;
+}
