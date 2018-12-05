1ae7d266f795f146f76d54116b3a1e3b410f61db
Michał Łępicki <michal.lepicki@gmail.com>
2018-12-05 02:20:36 +0100

    add day 4 part 2 elixir solution

--- 2018/elixir/4_repose_record.exs
+++ 2018/elixir/4_repose_record.exs
@@ -7,69 +7,81 @@ defmodule ReposeRecords do
     #   %{guard_id =>
     #      { minutes_asleep,
     #        %{minute => days_asleep},
-    #        { max_days_asleep_minute_so_far, minute_candidate }
+    #        { max_days_asleep, minute_candidate }
     #      },
-    #   { max_minutes_asleep_so_far, guard_candidate }
+    #   { max_minutes_asleep, guard_candidate },
+    #   { max_days_asleep, guard_candidate }
     # }
-    {_, _, records, {_, candidate}} =
-      Enum.reduce(records_strings, {nil, nil, %{}, {0, nil}}, &interpret_record_string/2)
+    {_, _, records, {_, candidate}, {_, candidate2}} =
+      Enum.reduce(
+        records_strings,
+        {nil, nil, %{}, {0, nil}, {0, nil}},
+        &interpret_record_string/2
+      )
 
     {_, _, {_, minute}} = Map.get(records, candidate)
-    candidate * minute
+    {_, _, {_, minute2}} = Map.get(records, candidate2)
+    {candidate * minute, candidate2 * minute2}
   end
 
   def interpret_record_string(
         <<_prefix::bytes-size(17)>> <> "] Guard #" <> suffix,
         acc
       ) do
-    {_, _, records, {max_so_far, candidate}} = acc
     {new_guard_id, _} = Integer.parse(suffix)
-    {new_guard_id, nil, records, {max_so_far, candidate}}
+    put_elem(acc, 0, new_guard_id)
   end
 
   def interpret_record_string(
         <<_prefix::bytes-size(15)>> <> <<minute::bytes-size(2)>> <> "] falls asleep\n",
         acc
       ) do
-    {guard_id, _, records, {max_so_far, candidate}} = acc
     minute = String.to_integer(minute)
-    {guard_id, minute, records, {max_so_far, candidate}}
+    put_elem(acc, 1, minute)
   end
 
   def interpret_record_string(
         <<_prefix::bytes-size(15)>> <> <<minute::bytes-size(2)>> <> "] wakes up\n",
         acc
       ) do
-    {guard_id, asleep_at, records, {max_so_far, candidate}} = acc
-    minute = String.to_integer(minute)
-    guards_records = Map.get(records, guard_id, {0, %{}, {0, nil}})
+    {guard_id, asleep_at, records, {max_minutes_asleep, candidate}, {max_days_asleep, candidate2}} =
+      acc
 
-    {count, _, _} =
-      updated_guards_records =
+    minute = String.to_integer(minute)
+    guard_record = Map.get(records, guard_id, {0, %{}, {0, nil}})
+
+    updated_guard_record =
+      {minutes_asleep, _, {days_asleep, _}} =
       Enum.reduce(
         asleep_at..(minute - 1),
-        guards_records,
-        fn minute, {asleep_count, minute_day_counts, {max_so_far, candidate}} ->
+        guard_record,
+        fn minute, {minutes_asleep, minute_day_counts, {max_days_asleep, minute_candidate}} ->
           minute_day_counts = Map.update(minute_day_counts, minute, 1, &(&1 + 1))
-          count = Map.get(minute_day_counts, minute)
+          days = Map.get(minute_day_counts, minute)
 
-          {new_max, new_candidate} =
-            case max(count, max_so_far) do
-              ^max_so_far -> {max_so_far, candidate}
-              ^count -> {count, minute}
+          sleepiest_minute =
+            case max(days, max_days_asleep) do
+              ^max_days_asleep -> {max_days_asleep, minute_candidate}
+              ^days -> {days, minute}
             end
 
-          {asleep_count + 1, minute_day_counts, {new_max, new_candidate}}
+          {minutes_asleep + 1, minute_day_counts, sleepiest_minute}
         end
       )
 
-    {new_max, new_candidate} =
-      case max(count, max_so_far) do
-        ^max_so_far -> {max_so_far, candidate}
-        ^count -> {count, guard_id}
+    strat1 =
+      case max(minutes_asleep, max_minutes_asleep) do
+        ^max_minutes_asleep -> {max_minutes_asleep, candidate}
+        ^minutes_asleep -> {minutes_asleep, guard_id}
       end
 
-    {guard_id, minute, Map.put(records, guard_id, updated_guards_records), {new_max, new_candidate}}
+    strat2 =
+      case max(days_asleep, max_days_asleep) do
+        ^max_days_asleep -> {max_days_asleep, candidate2}
+        ^days_asleep -> {days_asleep, guard_id}
+      end
+
+    {guard_id, minute, Map.put(records, guard_id, updated_guard_record), strat1, strat2}
   end
 end
 
@@ -77,4 +89,4 @@ end
 |> File.stream!()
 |> Enum.sort()
 |> ReposeRecords.solve()
-|> IO.puts()
+|> IO.inspect()
