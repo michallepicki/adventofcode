a791dd5124a5948b880cc4af20260d5c7d1c03bb
Michał Łępicki <michal.lepicki@gmail.com>
2018-12-05 12:11:15 +0100

    solve day 5 part 2 in elixir

--- 2018/elixir/5_alchemical_reduction.exs
+++ 2018/elixir/5_alchemical_reduction.exs
@@ -1,6 +1,29 @@
 #!/usr/bin/env elixir
 defmodule AlchemicalReduction do
   def solve(polymer_chain) do
+    initially_reduced = polymer_reduction(polymer_chain)
+    part_1 = length(initially_reduced)
+    alphabet = for n <- ?A..?Z, do: <<n::utf8>>
+
+    part_2 =
+      Enum.reduce(
+        alphabet,
+        part_1,
+        fn letter, min_length ->
+          min(
+            min_length,
+            initially_reduced
+            |> Enum.reject(&(String.upcase(&1) == letter))
+            |> polymer_reduction()
+            |> length()
+          )
+        end
+      )
+
+    {part_1, part_2}
+  end
+
+  def polymer_reduction(polymer_chain) do
     polymer_chain
     |> Enum.reduce(
       [],
@@ -15,7 +38,6 @@ defmodule AlchemicalReduction do
           end
       end
     )
-    |> length()
   end
 end
 
