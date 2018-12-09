b19326332a383686e19c9dea4e1e6b75e159a0b7
Michał Łępicki <michal.lepicki@gmail.com>
2018-12-09 13:45:32 +0100

    add day 7 part 1 elixir solution

---
+++ 2018/elixir/7_the_sum_of_its_parts.exs
@@ -0,0 +1,78 @@
+#!/usr/bin/env elixir
+defmodule TheSumOfItsParts do
+  def solve_a(instructions) do
+    graph =
+      instructions
+      |> Enum.reduce(
+        Map.new(),
+        fn {a, b}, graph ->
+          graph = Map.put_new(graph, a, [])
+          Map.update(graph, b, [a], fn required -> [a | required] end)
+        end
+      )
+
+    sort(graph)
+  end
+
+  def sort(graph), do: sort(graph, [])
+  def sort(graph, done) when map_size(graph) == 0, do: Enum.reverse(done)
+
+  def sort(graph, done) do
+    available = available_parts(graph) |> Enum.sort()
+    doing = hd(available)
+    graph = remove(graph, doing)
+    sort(graph, [doing | done])
+  end
+
+  def remove(graph, removed_part) do
+    graph
+    |> Enum.map(fn {part, required} -> {part, Enum.reject(required, &(&1 == removed_part))} end)
+    |> Enum.into(%{})
+    |> Map.delete(removed_part)
+  end
+
+  def available_parts(graph) do
+    Enum.filter(graph, fn {_part, required} -> required == [] end) |> Enum.map(&elem(&1, 0))
+  end
+
+  def read_file(filename) do
+    filename
+    |> File.stream!()
+    |> Enum.map(fn line ->
+      line
+      |> String.trim()
+      |> (fn "Step " <>
+               <<first::utf8>> <>
+               " must be finished before step " <> <<second::utf8>> <> " can begin." ->
+            {first, second}
+          end).()
+    end)
+  end
+end
+
+case System.argv() do
+  ["--test"] ->
+    ExUnit.start()
+
+    defmodule TheSumOfItsPartsTest do
+      use ExUnit.Case
+
+      test "example input" do
+        assert 'CABDGFE' =
+                 TheSumOfItsParts.solve_a([
+                   {?C, ?A},
+                   {?C, ?F},
+                   {?A, ?B},
+                   {?A, ?D},
+                   {?B, ?E},
+                   {?D, ?E},
+                   {?F, ?E},
+                   {?A, ?G}
+                 ])
+      end
+    end
+
+  _ ->
+    "../7" |> TheSumOfItsParts.read_file() |> TheSumOfItsParts.solve_a() |> IO.inspect()
+    # "../7" |> TheSumOfItsParts.read_file() |> ChronalCoordinates.solve_b() |> IO.inspect()
+end
