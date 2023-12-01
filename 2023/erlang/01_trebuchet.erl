43351917caed7207f8912df93f4787849c353aed
Michał Łępicki <michallepicki@users.noreply.github.com>
2023-12-01 18:05:11 +0100

    add types and specs to 23 erlang day 1

--- 2023/erlang/01_trebuchet.erl
+++ 2023/erlang/01_trebuchet.erl
@@ -8,17 +8,37 @@ main(_) ->
   erlang:display(part_one(FileContents, new_line, 0)),
   erlang:display(part_two(FileContents, new_line, 0)).
 
+
+-type state() :: new_line | complete_line_state().
+
+-type complete_line_state() :: {FirstDigit :: integer(), LastDigit :: integer()}.
+
+
+-spec part_one(
+  FileContents :: binary(),
+  State :: state(),
+  Acc :: integer()
+) -> Answer :: integer().
+
 part_one(<<Digit, Rest/binary>>, State, Acc) when Digit >= $0 andalso Digit =< $9 ->
   part_one(Rest, update_state(State, Digit), Acc);
 
-part_one(<<$\n, Rest/binary>>, {FirstDigit, LastDigit}, Acc) ->
-  part_one(Rest, new_line, update_acc(FirstDigit, LastDigit, Acc));
+part_one(<<$\n, Rest/binary>>, State, Acc) ->
+  part_one(Rest, new_line, update_acc(State, Acc));
 
 part_one(<<_, Rest/binary>>, State, Acc) ->
   part_one(Rest, State, Acc);
 
 part_one(<<>>, new_line, Acc) ->
   Acc.
+
+
+
+-spec part_two(
+  FileContents :: binary(),
+  State :: state(),
+  Acc :: integer()
+) -> Answer :: integer().
 
 part_two(<<Digit, Rest/binary>>, State, Acc) when Digit >= $0 andalso Digit =< $9 ->
   part_two(Rest, update_state(State, Digit), Acc);
@@ -50,8 +70,8 @@ part_two(<<"eight", Rest/binary>>, State, Acc) ->
 part_two(<<"nine", Rest/binary>>, State, Acc) ->
   part_two(<<"e", Rest/binary>>, update_state(State, $9), Acc);
 
-part_two(<<$\n, Rest/binary>>, {FirstDigit, LastDigit}, Acc) ->
-  part_two(Rest, new_line, update_acc(FirstDigit, LastDigit, Acc));
+part_two(<<$\n, Rest/binary>>, State, Acc) ->
+  part_two(Rest, new_line, update_acc(State, Acc));
 
 part_two(<<_, Rest/binary>>, State, Acc) ->
   part_two(Rest, State, Acc);
@@ -59,11 +79,19 @@ part_two(<<_, Rest/binary>>, State, Acc) ->
 part_two(<<>>, new_line, Acc) ->
   Acc.
 
+
+
+-spec update_state(CurrentState :: state(), Digit :: integer()) -> NewState :: state().
+
 update_state(new_line, Digit) ->
   {Digit, Digit};
 
 update_state({FirstDigit, _LastDigit}, Digit) ->
   {FirstDigit, Digit}.
 
-update_acc(FirstDigit, LastDigit, Acc) ->
+
+
+-spec update_acc(State :: complete_line_state(), CurrentAcc :: integer()) -> NewAcc :: integer().
+
+update_acc({FirstDigit, LastDigit}, Acc) ->
   Acc + 10 * (FirstDigit - $0) + (LastDigit - $0).
